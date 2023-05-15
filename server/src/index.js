d48be2e061d947088b12676b0a8ec7030566e876
20120434 <meikaruku@gmail.com>
2023-05-15 15:42:13 +0700

    update port client

--- server/src/index.js
+++ server/src/index.js
@@ -16,7 +16,7 @@ app.use(cookieParser());
 app.use(express.json());
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", `http://localhost:${port}`);
+  res.setHeader("Access-Control-Allow-Origin", `http://localhost:3000`);
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type");
   res.setHeader("Access-Control-Allow-Credentials", true);
