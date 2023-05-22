ab2319a1d14cd23cb408bcc16c4361039bff98cc
20120434 <meikaruku@gmail.com>
2023-05-22 08:47:32 +0700

    fix /user/changePassword

--- server/src/app/controllers/user.c.js
+++ server/src/app/controllers/user.c.js
@@ -1,5 +1,7 @@
 const userModel = require("../models/user.m");
 
+const bcrypt = require("bcrypt");
+
 const userController = {
   // [GET] /profile?id={id_user}
   getProfile: async (req, res) => {
@@ -39,15 +41,25 @@ const userController = {
   // [POST] /profile
   changePassword: async (req, res) => {
     try {
-      const infoUser = {
-        id: req.body.id,
-        password: req.body.password,
-      };
-      const updatedInfoUser = await userModel.changePassword(infoUser);
+      // check old password
+      const user = await userModel.getUserByID(req.body.id);
+      const validPassword = await bcrypt.compare(req.body.oldPassword, user.password);
+      if (!validPassword) {
+        return res.status(400).json("Incorrect Old Password!");
+      } else {
+        // hash password
+        const salt = await bcrypt.genSalt(11);
+        const hashedNewPass = await bcrypt.hash(req.body.newPassword, salt);
 
-      const { password, ...others } = updatedInfoUser;
+        // create user with new password
+        const infoUser = {
+          id: req.body.id,
+          password: hashedNewPass,
+        };
 
-      res.status(200).json(others);
+        await userModel.changePassword(infoUser);
+        return res.status(200).json("Change Password Successfully!");
+      }
     } catch (error) {
       res.status(500).json(error);
     }
