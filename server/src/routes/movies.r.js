daa5d306431aeb1564cdd929b8c4789e2f1a4ec2
20120434 <meikaruku@gmail.com>
2023-05-23 16:31:19 +0700

    add swagger for route /movies

--- server/src/routes/movies.r.js
+++ server/src/routes/movies.r.js
@@ -3,9 +3,106 @@ const middlewareController = require("../middleware/middleware.js");
 
 const router = require("express").Router();
 
-router.get("/getListMovies", moviesController.getListMovies);
-router.get("/getDetail", moviesController.getDetail);
-router.get("/getCurrentMovies", moviesController.getCurrentMovies);
-router.get("/getInComingMovies", moviesController.getInComingMovies);
+/**
+ * @swagger
+ * tags:
+ *   name: /movies
+ *   description: API for movies
+ */
+
+/**
+ * @swagger
+ * /movies/listMovies:
+ *  get:
+ *   summary: get all movies
+ *   tags: [/movies]
+ *   responses:
+ *     '200':
+ *       description: get list all movies successfully
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               id:
+ *                 type: integer
+ *                 description: movie's id
+ *               title:
+ *                 type: string
+ *                 description: movie's title
+ *               release_date:
+ *                 type: string
+ *                 description: movie's release date
+ *               url_poster:
+ *                 type: string
+ *                 description: movie's url poster
+ *     '500':
+ *       description: Internal server error
+ */
+router.get("/listMovies", moviesController.getListMovies);
+
+/**
+ * @swagger
+ * /movies/detail?id={id}:
+ *  get:
+ *   summary: get movie's detail by id
+ *   tags: [/movies]
+ *   parameters:
+ *     - name: id
+ *       in: path
+ *       description: Movie's ID
+ *       required: true
+ *       type: integer
+ *   responses:
+ *     '200':
+ *       description: Register successfully
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/MovieDetail'
+ *     '500':
+ *       description: Internal server error
+ */
+router.get("/detail", moviesController.getDetail);
+
+/**
+ * @swagger
+ * /movies/currentMovies:
+ *  get:
+ *   summary: get all current movies
+ *   tags: [/movies]
+ *   responses:
+ *     '200':
+ *       description: Register successfully
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: array
+ *             items:
+ *               $ref: '#/components/schemas/MovieDetail'
+ *     '500':
+ *       description: Internal server error
+ */
+router.get("/currentMovies", moviesController.getCurrentMovies);
+
+/**
+ * @swagger
+ * /movies/inComingMovies:
+ *  get:
+ *   summary: get all in coming movies
+ *   tags: [/movies]
+ *   responses:
+ *     '200':
+ *       description: Register successfully
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: array
+ *             items:
+ *               $ref: '#/components/schemas/MovieDetail'
+ *     '500':
+ *       description: Internal server error
+ */
+router.get("/inComingMovies", moviesController.getInComingMovies);
 
 module.exports = router;
