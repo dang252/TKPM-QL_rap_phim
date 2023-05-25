bec50b060abdcfe73d41d3ac0ff8aaa465e82a61
20120434 <meikaruku@gmail.com>
2023-05-25 14:06:00 +0700

    fix bookingHistory

--- server/src/app/models/user.m.js
+++ server/src/app/models/user.m.js
@@ -53,11 +53,14 @@ module.exports = {
   },
   bookingHistory: async (id_user) => {
     try {
-      const rs = await db.any("SELECT c.name, c.location, b.purchase_date, m.title, s.time, m.duration, r.name, b.id_seats, b.id_food_drink, b.total_price FROM book b JOIN tickets t ON b.id_ticket = t.id JOIN rooms r ON t.id_room = r.id JOIN cinemas c ON r.id_cinema = c.id JOIN schedule s ON b.id_schedule = s.id JOIN movies m ON s.id_movie = m.id WHERE id_user = $1;", [id_user]);
+      const rs = await db.any("SELECT * FROM get_booking_history($1);", [id_user]);
       return rs;
     } catch (err) {
-      console.log("Error in bookingHistory in user.m: ", err);
-      return null;
+      if (err.code === 0) {
+        return null;
+      } else {
+        throw err;
+      }
     }
   },
 };
