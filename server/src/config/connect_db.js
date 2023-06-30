dbff8d1539b997caaab0e1687d0a7b5b0b633625
20120050 <dang_252@yahoo.com.vn>
2023-06-30 17:00:04 +0700

    chỉnh sửa các file config DB đểthuận tiện cho việc deploy

--- server/src/config/connect_db.js
+++ server/src/config/connect_db.js
@@ -3,12 +3,12 @@ const pgp = require("pg-promise")(initOptions);
 require("dotenv").config();
 
 const cn = {
-  host: "localhost",
-  port: 5432,
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
   database: process.env.DATABASE_NAME,
-  user: "postgres",
+  user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
-  max: 30,
+  max: process.env.DATABASE_MAX,
 };
 
 module.exports = pgp(cn);
